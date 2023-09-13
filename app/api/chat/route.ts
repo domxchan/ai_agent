51b8ac4331cc5d707d94000a9614d431a2198cde
Dom Chan <dominic.chan@gmail.com>
2023-09-13 11:51:40 +0800

    added OpenAiAgentTokenBufferMemory

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,6 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { StreamingTextResponse, LangChainStream, Message } from 'ai';
-import { AIMessage, HumanMessage } from 'langchain/schema';
+import { AIMessage, HumanMessage, ChatMessage } from 'langchain/schema';
 import {
   DynamicTool,
   DynamicStructuredTool,
@@ -9,7 +10,9 @@ import {
 } from 'langchain/tools';
 import { Calculator } from 'langchain/tools/calculator';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { createRetrieverTool, OpenAIAgentTokenBufferMemory } from 'langchain/agents/toolkits';
 import { initializeAgentExecutorWithOptions } from 'langchain/agents';
+import { ChatMessageHistory } from 'langchain/memory';
 
 import { getVectorStoreChain } from '../../../lib/utils/vectorStoreChain.js';
 
@@ -17,10 +20,24 @@ import * as z from 'zod';
 
 // export const runtime = 'edge';
 
+const convertVercelMessageToLangChainMessage = (message: Message) => {
+  if (message.role === 'user') {
+    return new HumanMessage(message.content);
+  } else if (message.role === 'assistant') {
+    return new AIMessage(message.content);
+  } else {
+    return new ChatMessage(message.content, message.role);
+  }
+};
+
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const body = await req.json();
 
   // const { stream, handlers } = LangChainStream();
+
+  const messages = (body.messages ?? []).filter(
+    (message: Message) => message.role === 'user' || message.role === 'assistant'
+  );
 
   const model = new ChatOpenAI({
     temperature: 0,
@@ -86,32 +103,72 @@ export async function POST(req: Request) {
     tools.push(docQaTool);
   }
 
+  const returnIntermediateSteps = body.show_intermediate_steps;
+  const previousMessages = messages.slice(0, -1);
+  const currentMessageContent = messages[messages.length - 1].content;
+
+  // const currentMessageContent = messages[messages.length - 1].content;
+  console.log('messages: ', messages);
+  console.log(currentMessageContent);
+
+  const chatHistory = new ChatMessageHistory(
+    previousMessages.map(convertVercelMessageToLangChainMessage)
+  );
+
+  /**
+   * This is a special type of memory specifically for conversational
+   * retrieval agents.
+   * It tracks intermediate steps as well as chat history up to a
+   * certain number of tokens.
+   *
+   * The default OpenAI Functions agent prompt has a placeholder named
+   * "chat_history" where history messages get injected - this is why
+   * we set "memoryKey" to "chat_history". This will be made clearer
+   * in a future release.
+   */
+  const memory = new OpenAIAgentTokenBufferMemory({
+    llm: model,
+    memoryKey: 'chat_history',
+    outputKey: 'output',
+    chatHistory,
+  });
+
   const executor = await initializeAgentExecutorWithOptions(tools, model, {
     agentType: 'openai-functions',
+    memory,
+    returnIntermediateSteps: true,
     verbose: true,
+    // callbacks: [handlers],
   });
 
-  const input = messages[messages.length - 1].content;
-  console.log('messages: ', messages);
-  console.log(input);
+  // const result = await executor.run(currentMessageContent);
+  const result = await executor.call({ input: currentMessageContent });
 
-  const result = await executor.run(input);
+  // console.log('result: ', result);
+  console.log('result: ', result.output);
+  const chunks = result.output.split(' ');
 
-  const chunks = result.split(' ');
+  if (returnIntermediateSteps) {
+    return NextResponse.json(
+      { output: result.output, intermediate_steps: result.intermediateSteps },
+      { status: 200 }
+    );
+  } else {
+    const responseStream = new ReadableStream({
+      async start(controller) {
+        for (const chunk of chunks) {
+          const bytes = new TextEncoder().encode(chunk + ' ');
+          controller.enqueue(bytes);
+          await new Promise((r) => setTimeout(r, Math.floor(Math.random() * 20 + 10)));
+        }
+        controller.close();
+      },
+    });
 
-  const responseStream = new ReadableStream({
-    async start(controller) {
-      for (const chunk of chunks) {
-        const bytes = new TextEncoder().encode(chunk + ' ');
-        controller.enqueue(bytes);
-        await new Promise((r) => setTimeout(r, Math.floor(Math.random() * 20 + 10)));
-      }
-      controller.close();
-    },
-  });
+    console.log('responseStream: ', responseStream);
 
-  return new StreamingTextResponse(responseStream);
-
+    return new StreamingTextResponse(responseStream);
+  }
   // model
   //   .call(
   //     (messages as Message[]).map((m) =>
